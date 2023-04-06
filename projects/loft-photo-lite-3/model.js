974a59c1302d37de443f769e4a15c793c82d3489
Sergey Cherniy <antkrid@gmail.com>
2023-04-06 10:58:46 +0300

    выполнил задание 4 недели

--- projects/loft-photo-lite-3/model.js
+++ projects/loft-photo-lite-3/model.js
@@ -1,21 +1,103 @@
+const PERM_FRIENDS = 2;
+const PERM_PHOTOS = 4;
+const APP_ID = 51605748;
+
 export default {
-  getRandomElement(array) {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
 
-  async getNextPhoto() {},
+    const index = Math.round(Math.random() * (array.length - 1));
 
-  login() {},
+    return array[index];
+  },
 
-  init() {},
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
 
-  photoCache: {},
-  getFriendPhotos(id) {
-    const photos = this.photoCache[id];
+    return { friend, id: photo.id, url: size.url };
+  },
+
+  findSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+    
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+        
+        return biggest;
+      }, photo.sizes[0]);
+    };
+
+    return size;
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+  },
+
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId: APP_ID,
+      });
+      
+      VK.Auth.login((response) => {
+        if (response.session) {
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+  callApi(method, params) {
+    params.v = params.v || '5.131';
+
+    return new Promise((resolve, reject) => {
+      VK.api(method, params, (response) => {
+        if (response.error) {
+          reject(new Error(response.error.error_msg));
+        } else {
+          resolve(response.response);
+        }
+      });
+    });
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    };
+  
+    return this.callApi('photos.getAll', params);
+  },
+
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
 
     if (photos) {
       return photos;
     }
 
-    // const photos = вместо этого комментария вставьте код для получения фотографии друга из ВК
+    photos = await this.getPhotos(id);
 
     this.photoCache[id] = photos;
 
