e08420613165138275198c31d40ecaaee07e922c
Sergey Cherniy <antkrid@gmail.com>
2023-04-12 23:55:23 +0300

    выполнил задание 5 недели

--- projects/loft-photo-lite-4/mainPage.js
+++ projects/loft-photo-lite-4/mainPage.js
@@ -9,7 +9,47 @@ export default {
   },
 
   setFriendAndPhoto(friend, id, url) {
+    const photoComp = document.querySelector('.component-photo');
+    const headerPhotoComp = document.querySelector('.component-header-photo');
+    const headerNameComp = document.querySelector('.component-header-name');
+    const footerPhotoComp = document.querySelector ('.component-footer-photo');
+
+    this.friend = friend;
+
+    headerPhotoComp.style.backgroundImage = `url('${friend.photo_50}')`;
+    headerNameComp.innerText = `${friend.first_name ?? ''} ${friend.last_name ?? ''}`;
+    photoComp.style.backgroundImage = `url(${url})`;
+    footerPhotoComp.style.backgroundimage = `url('${model.me.photo_50}')`;
   },
 
   handleEvents() {
+    let startFrom;
+
+    document.querySelector('.component-photo').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        startFrom = { y: e.changedTouches[0].pageY };
+    });
+
+    document.querySelector('.component-photo').addEventListener('touchend', async (e) => {
+      const direction = e.changedTouches[0].pageY - startFrom.y;
+
+      if (direction < 0) {
+        await this.getNextPhoto();
+      }
+    });
+
+    document
+      .querySelector('.component-header-profile-link')
+      .addEventListener('click', async () => {
+        await profilePage.setUser(this.friend);
+        pages.openPage('profile');
+      });
+
+    document 
+      .querySelector('.component-footer-container-profile-link')
+      .addEventListener('click', async () => {
+        await profilePage.setUser(model.me);
+        pages.openPage('profile');
+      });
+  },
 };
--- projects/loft-photo-lite-4/model.js
+++ projects/loft-photo-lite-4/model.js
@@ -1,21 +1,123 @@
 const PERM_FRIENDS = 2;
 const PERM_PHOTOS = 4;
-const APP_ID = 5350105;
+const APP_ID = 51605748;
 
 export default {
-  getRandomElement(array) {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
 
-  async getNextPhoto() {},
+    const index = Math.round(Math.random() * (array.length - 1));
 
-  async init() {},
+    return array[index];
+  },
 
-  login() {},
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
 
-  logout() {},
+    return { friend, id: photo.id, url: size.url };
+  },
 
-  getFriends() {},
+  findSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+    
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+        
+        return biggest;
+      }, photo.sizes[0]);
+    };
 
-  getUsers(ids) {},
+    return size;
+  },
 
-  async getFriendPhotos(id) {},
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+    [this.me] = await this.getUsers();
+  },
+
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId: APP_ID,
+      });
+      
+      VK.Auth.login((response) => {
+        if (response.session) {
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+  logout() {
+    return new Promise((resolve) => VK.Auth.revokeGrants(resolve));
+  },
+
+  callApi(method, params) {
+    params.v = params.v|| '5.131';
+
+    return new Promise((resolve, reject) => {
+      VK.api(method, params, (response) => {
+        if (response.error) {
+          reject(new Error(response.error.error_msg));
+        } else {
+          resolve(response.response);
+        }
+      });
+    });
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    };
+    
+    return this.callApi('photos.getAll', params);
+  },
+
+  getUsers(ids) {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    if(ids) {
+      params.useer_ids = ids;
+    }
+
+    return this.callApi('users.get', params);
+  },
+
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
+
+    if (photos) {
+      return photos;
+    }
+
+    photos = await this.getPhotos(id);
+
+    this.photoCache[id] = photos;
+
+    return photos;
+  },
 };
